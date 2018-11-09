713d2f0d1c6b51df3dd733e5e77d8c7d1bfe6e09
Angelo Corsaro <angelo@icorsaro.net>
2018-11-09 12:23:56 +0100

    removed some files that had accidentally slipped in

--- labs/01/src/main/scala/edu/esiee/scala18/Complex.scala
+++ labs/01/src/main/scala/edu/esiee/scala18/Complex.scala
@@ -1,9 +1,13 @@
 package edu.esiee.scala18
-
+// import edu.esiee.scala18._  
 object Complex {
   def apply(re: Double, im: Double) = new Complex(re, im)
 }
 
 class Complex(val re: Double, val im: Double) {
+  def + (b: Complex) = Complex(this.re + b.re, this.im + b.im)
+
+  def sum (b: Complex) = Complex(this.re + b.re, this.im + b.im)
+
   override def toString = s"$re+i$im"
 }
